879e4491ae7569883b849910d0af7086e59e986d
Leandro Cavalcanti <leand@DESKTOP-LFIP8ID.(none)>
2020-12-11 08:41:13 -0300

    sobrecarga

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using POO_Polimorfismo_Sobrecarga.Classes;
+
+namespace POO_Polimorfismo_Sobrecarga
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Funcionario fun = new Funcionario();
+            fun.Mostrar();
+            fun.Mostrar("Tsuka");
+            fun.Mostrar(3);
+
+            Console.Beep();
+        }
+    }
+}
